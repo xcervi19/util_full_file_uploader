d597fa07c851249f5730f3bc26cd4582e24e911a
karel cervicek <cervicekkarel@gmail.com>
2024-03-11 17:55:51 +0100

    fix template

--- util_full_file_uploader/app/uploadzone/[path]/page.tsx
+++ util_full_file_uploader/app/uploadzone/[path]/page.tsx
@@ -2,11 +2,8 @@ import { useRouter } from "next/router";
 import Image from "next/image";
 import { useEffect, useState } from "react";
 import { useParams } from "next/navigation";
-import { getImagesUrls } from "@/src/helpers/imageUtils";
-import Gallery from "@/src/components/Gallery";
 
-export default function GalleryPage({ params }) {
-  const images = getImagesUrls(params.path);
+export default function UploadzonePage() {
   return (
     <div className="grid grid-cols-1 md:grid-cols-2 gap-4 px-4 py-6">
       <p>
